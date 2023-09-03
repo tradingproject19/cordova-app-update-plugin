15fe0a169944f3b671feea78b5d513eb38a2edf3
tradingproject19 <50413570+tradingproject19@users.noreply.github.com>
2023-09-03 21:52:59 +0500

    Update updatePlugin.js

--- www/updatePlugin.js
+++ www/updatePlugin.js
@@ -1,21 +1,21 @@
 // Empty constructor
 var exec = require('cordova/exec');
 
-function UpdatePlugin() {}
+function InAppUpdate() {}
 
 // The function that passes work along to native shells
 // Message is a string, duration may be 'long' or 'short'
-UpdatePlugin.prototype.update = function(successCallback, errorCallback, config) {
+InAppUpdate.prototype.update = function(successCallback, errorCallback, config) {
   exec(successCallback, errorCallback, 'UpdatePlugin', 'update', [{ 'flexibleUpdateStalenessDays': config.flexibleUpdateStalenessDays, 'immediateUpdateStalenessDays': config.immediateUpdateStalenessDays }]);
 }
 
 // Installation constructor that binds updatePlugin to window
-UpdatePlugin.install = function() {
+InAppUpdate.install = function() {
   if (!window.plugins) {
     window.plugins = {};
   }
-  window.plugins.updatePlugin = new UpdatePlugin();
-  return window.plugins.updatePlugin;
+  window.plugins.InAppUpdate = new InAppUpdate();
+  return window.plugins.InAppUpdate;
 };
 
-cordova.addConstructor(UpdatePlugin.install);
+cordova.addConstructor(InAppUpdate.install);
